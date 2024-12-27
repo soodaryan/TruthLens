e9a8aaa3ddee2320a36177b4466fdfc596452048
hemangjain17 <hemangjain_23cs174@dtu.ac.in>
2024-12-28 00:59:37 +0530

    Audio Sentiment Model

--- audio/SpeechToText/text_to_image.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import InferenceClient
-client = InferenceClient("black-forest-labs/FLUX.1-dev", token="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")
-
-# output is a PIL.Image object
-image = client.text_to_image("Astronaut riding a horse")
-image.show()
